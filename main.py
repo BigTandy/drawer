32e9906f2f52836e156666174a94853a5586f051
BPLP <Buddyjack264@GMAIL.COM>
2023-01-19 09:16:44 -0500

    Adding Copywrite Header

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Copyright Bud Patterson, 2022, 2023
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. 
+
+
 import tkinter as tk
 import random as rand
 import asyncio
